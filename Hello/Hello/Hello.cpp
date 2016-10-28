13ae174e4ada4b8afb4fad06b5c1fbf7401d5381
rojashe <herofo2040@gmail.com>
2016-10-28 09:04:26 -0600

    Adding visual studio files

---
+++ Hello/Hello/Hello.cpp
@@ -0,0 +1,13 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!!!\n";
+	getchar();
+    return 0;
+}
+
