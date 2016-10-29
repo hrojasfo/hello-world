2298c7feb6916bb0258056d9e506c5ef52e994c7
Henry <herofo2040@gmail.com>
2016-10-29 10:42:39 -0600

    Adding greetings

--- Hello/Hello/Hello.cpp
+++ Hello/Hello/Hello.cpp
@@ -3,10 +3,15 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
+	std::string name;
 	std::cout << "Hello World!!!\n";
+	std::cout << "Who are you? ";
+	std::cin >> name;
+	std::cout << "Hello " << name << '\n';
 	getchar();
     return 0;
 }
